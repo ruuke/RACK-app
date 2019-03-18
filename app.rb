58209d95a414e31519da140cec1d27f539c460f3
ruuke <ruuke@mail.ru>
2019-03-18 14:22:51 +0500

    1st working version

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ class App
 
   def call(env)
     @env = env
+    time_now
     [status, headers, body]
   end
 
   private
 
   def status
-    200
+    @status
   end
 
   def headers
@@ -21,22 +22,25 @@ class App
 
   def time_now
     path = @env['REQUEST_PATH']
-    params, options = (@env['QUERY_STRING']).split('=')    
-    time = []
+    params, options = (@env['QUERY_STRING']).split('=')
+    @time = []
+    @error = []
     if path == "/time" && params == "format"
-      begin
-        options.split('%2C').each do |option|
-          time << Time.now.method(option).call
+      options.split('%2C').each do |option|
+        begin         
+        @time << Time.now.method(option).call
+                
+        rescue NameError
+          @error << option
         end
-        time.join('-')
-      rescue NameError => e
-        "Unknown time format #{e}"
       end
-      
-      else
-        options.split('%2')
-        formatt
-      end
+
+    else
+      @status = 404
+      return
+    end
+    @error.empty? ? @status = 200 : @status = 400
+    @error.empty? ? @body = @time.join('-') : @body = "Unknown time format #{@error}"
   end
 
 end
